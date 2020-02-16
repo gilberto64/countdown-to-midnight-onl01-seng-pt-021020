aa74d73323515f4411a13e442ac0212a93023efb
Gilberto Ocampo <ocampo@gate.net>
2020-02-16 17:52:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,13 @@ def countdown(num)
   'HAPPY NEW YEAR!'
 
 def countdown_with_sleep(num)
+ while num > 0 
+    puts "#{num} SECOND(S)!"
+    num -= 1 
+
+  end
+  
+  'HAPPY NEW YEAR!'
 
 
 sleep(1)
